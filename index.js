c43d4fc688eb691a8125aaeae836f6f65567b6c7
flyingjjang051 <flyjjang051@gmail.com>
2022-08-16 12:20:37 +0900

    chatting room

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express"); // 웹서버
 const http = require("http"); // node에 원래 부터  있던 모듈
 const path = require("path"); // node에 원래 부터 있던 모듈  서버가 구동되는 폴더의 절대 경로 알려주는 모듈
 const app = express();
+const moment = require("moment");
 const socketIO = require("socket.io");
 const server = http.createServer(app);
 const io = socketIO(server); // 여기서 클라이언트로 데이터 내려줌....
@@ -11,12 +12,14 @@ const PORT = app.get("port");
 
 io.on("connection", (socket) => {
   console.log("클라이언트 연결 되었습니다.");
-  socket.on("yaho", (data) => {
-    console.log(data);
-    io.emit("serverYaho", "나는  클라이언트가 yaho로 보낸 이벤트를 받아서 다시 yaho로 서버가 다시 yaho로 이벤트를 발송하는 것입니다.");
-  });
+  // socket.on("yaho", (data) => {
+  //   console.log(data);
+  //   io.emit("serverYaho", "나는  클라이언트가 yaho로 보낸 이벤트를 받아서 다시 yaho로 서버가 다시 yaho로 이벤트를 발송하는 것입니다.");
+  // });
   socket.on("chatting", (data) => {
     console.log(data);
+    const sendTime = moment(new Date()).format("A hh:mm");
+    io.emit("chatting", { nickName: data.nickName, msg: data.msg, time: sendTime });
   });
 });
 app.get("/", (req, res) => {
